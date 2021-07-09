dee1b412486aa9978ad2b2589ef8dd45b106126c
Karpovich_alex_mpc <karpovehalex@gmail.com>
2021-07-09 16:48:41 +0300

    Add rabbit to config and start making compose

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,4 +7,5 @@ load_dotenv()
 
 class Config:
     TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+    RABBITMQ = os.environ.get("RABBITMQ") or ''
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///:memory:?check_same_thread=False'
